cd33dd37b0fc2da5de6eb570102c5ad3381de9a7
Srieam Tharividi <139240678+sriram5N2@users.noreply.github.com>
2026-08-03 22:19:18 +0530

    Implemented the Transfer Method in the Transaction Service

--- transaction-service/src/main/java/com/bankingsystem/transactionservice/client/AccountServiceClient.java
+++ transaction-service/src/main/java/com/bankingsystem/transactionservice/client/AccountServiceClient.java
@@ -1,5 +1,16 @@
 package com.bankingsystem.transactionservice.client;
 
-public class AccountServiceClient {
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.math.BigDecimal;
+
+@FeignClient(name="account-service",url="${(account.service.url)}")
+public interface AccountServiceClient {
+
+    @PutMapping("/api/v1/accounts/{accountNumber}/deduct")
+    String deductBalance(@PathVariable String accountNumber, @RequestBody BigDecimal amount);
     
 }
--- transaction-service/src/main/java/com/bankingsystem/transactionservice/service/TransactionService.java
+++ transaction-service/src/main/java/com/bankingsystem/transactionservice/service/TransactionService.java
@@ -1,16 +1,24 @@
 package com.bankingsystem.transactionservice.service;
 
+import com.bankingsystem.transactionservice.client.AccountServiceClient;
+import com.bankingsystem.transactionservice.dto.TransactionResponse;
+import com.bankingsystem.transactionservice.dto.TransferRequest;
+import com.bankingsystem.transactionservice.entity.Transaction;
+import com.bankingsystem.transactionservice.entity.TransactionStatus;
+import com.bankingsystem.transactionservice.entity.TransactionType;
 import com.bankingsystem.transactionservice.repository.TransactionRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class TransactionService {
+public class TransactionService  {
     private final TransactionRepository transactionRepository;
-
+    private final AccountServiceClient accountServiceClient;
 
     private static final String Transaction_INITIATED_TOPIC="transaction_initiated";
     private static final String Transaction_COMPLETED_TOPIC_2="transaction_completed";
@@ -22,7 +30,31 @@ public class TransactionService {
      * saves transaction as processing
      * publish event to kafka for fraud check
      * Returns
-     * @param request
+     * @paramrequest
      * @return
      */
+
+    public TransactionResponse transfer (TransferRequest request)
+    {
+        accountServiceClient.deductBalance(
+                request.getSenderAccountNumber(),request.getAmount());
+
+        Transaction transaction = new Transaction();
+        transaction.setSenderAccountNumber(request.getSenderAccountNumber());
+        transaction.setAmount(request.getAmount());
+        transaction.setReceiverAccountNumber(request.getRecipientAccountNumber());
+        transaction.setStatus(TransactionStatus.PROCESSING);
+        transaction.setType(TransactionType.TRANSFER);
+        transaction.setDescription(request.getDescription());
+        transaction.setRefernceNumber(UUID.randomUUID().toString());
+
+        Transaction savedTransaction = transactionRepository.save(transaction);
+
+        log.info("Transaction saved as Processing:{}",savedTransaction.getTransactionId());
+
+    }
+
+
+
+
 }
